56b639a99a4da6b1206224c0aa2202c3e1556489
Rohit <rohitkumar252526@gmail.com>
2023-02-21 19:21:58 +0530

    New Updates

--- stringshift.cpp
+++ stringshift.cpp
@@ -7,23 +7,16 @@ int main(){
 
     // Write your code here......
 
-    char str[50] = {'A','B','C','D','\0'};
-    char t1 = 'E', t2 = 'F';
-    int i=0;
-
+    char str[50] = "abcd";
+    cout << str << endl;
+    int i = 1;
     while(str[i] != '\0'){
-        cout << str[i] << " ";
+        str[i-1] = str[i];
         i++;
     }
-    i = 4;
-    while(i>0){
-        str[i+1] = str[i];
-        i--;
-    }
-     while(str[i] != '\0'){
-        cout << str[i] << endl;
-        i++;
-    }
+    str[i-1] = str[i];
+    cout << str << endl;
+   
 
     return 0;
 }
